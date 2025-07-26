537903556c9be51e630f2a1afa108e33d064297f
Favourfisayo <Favourfisayo5@gmail.com>
2025-07-26 22:33:02 +0100

    Implemented Server actions, server-side form validation, Search function with search params, and handling errors

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from "react"
 import FullBlogPost from "@/app/ui/Components/FullBlogPost"
 import { SingleBlogSkeleton } from "@/app/ui/skeleton"
-const page = (props: {params: {id: number}}) => {
+const page = (props: {params: {id: string}}) => {
   const blog_param = props.params
-  const blog_id = blog_param.id
+  const blog_id = parseInt(blog_param.id, 10)
 
   return (
     <>
